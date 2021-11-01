dae7a302223851ad596300d8ee43ca94be2c9397
youssef shaaban <ysfme2@hotmail.com>
2021-11-01 03:04:34 +0200

    Add project files.

---
+++ Image Classification Test/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Image_Classification_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(304, 27);
+            this.txtPath.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtResult.Location = new System.Drawing.Point(12, 53);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(304, 27);
+            this.txtResult.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 27);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Get Result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 92);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Image Classification Test/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Classification_Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                txtPath.Text = openFile.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPath.Text) || !File.Exists(txtPath.Text))
+                return;
+            ModelInput sampleData = new ModelInput()
+            {
+                ImageSource = txtPath.Text,
+            };
+            // Make a single prediction on the sample data and print results
+            txtResult.Text = ConsumeModel.Predict(sampleData).Prediction;
+        }
+    }
+}
